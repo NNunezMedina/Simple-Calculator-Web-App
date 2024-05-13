2f20c374adcc1cae38fc84ba1d164cc8099e808e
NNunez <nathalynunezmedina@gmail.com>
2024-05-13 11:44:44 -0500

    Putting more operations

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 const firstNumberInput = document.querySelector('#num1');
 const secondNumberInput = document.querySelector('#num2');
 const resultInput = document.querySelector('#result');
-const addButton = document.querySelector('#add')
+const addButton = document.querySelector('#add');
+const subtractionButton = document.querySelector('#subtraction');
+const multiplicateButton = document.querySelector('#multiplicate');
+const divideButton = document.querySelector('#divide');
+
 
 function addNumbers() {
     const firstNumber = parseFloat(firstNumberInput.value)
@@ -10,6 +14,30 @@ function addNumbers() {
     resultInput.value = result;
 }
 
-addButton.addEventListener("click",addNumbers)
+function subtractionNumbers() {
+    const firstNumber = parseFloat(firstNumberInput.value)
+    const secondNumber = parseFloat(secondNumberInput.value)
+    const result = firstNumber - secondNumber;
+    resultInput.value = result;
+}
+
+function multiplicateNumbers() {
+    const firstNumber = parseFloat(firstNumberInput.value)
+    const secondNumber = parseFloat(secondNumberInput.value)
+    const result = firstNumber * secondNumber;
+    resultInput.value = result;
+}
+
+function divideNumbers() {
+    const firstNumber = parseFloat(firstNumberInput.value)
+    const secondNumber = parseFloat(secondNumberInput.value)
+    const result = firstNumber / secondNumber;
+    resultInput.value = result.toFixed(5);
+}
+
+addButton.addEventListener("click",addNumbers);
+subtractionButton.addEventListener("click",subtractionNumbers);
+multiplicateButton.addEventListener("click", multiplicateNumbers);
+divideButton.addEventListener("click", divideNumbers);
 
 
